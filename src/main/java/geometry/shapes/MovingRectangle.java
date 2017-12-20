6737ed1603a479086fce6839b695bfd799cb9b5b
Sasha Kaminska <sasha249133@ukr.net>
2017-12-21 00:08:41 +0200

    add Moving Rectangle class

--- src/main/java/geometry/shapes/MovingRectangle.java
+++ src/main/java/geometry/shapes/MovingRectangle.java
@@ -1,64 +1,66 @@
 package geometry.shapes;
 
-import javafx.scene.canvas.GraphicsContext;
+import javafx.application.Platform;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import random.RandomGenerator;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class MovingRectangle implements Runnable {
-    private final int WIDTH;
-    private final int HEIGHT;
-    private GraphicsContext gc;
-    private Rectangle rectangle = new Rectangle();
-    private int distance = 30;
-    private int x;
-    private int y;
-
-    public MovingRectangle(int WIDTH, int HEIGHT, GraphicsContext gc, int minW, int maxW, int minH, int maxH) {
-        this.WIDTH = WIDTH;
-        this.HEIGHT = HEIGHT;
-        this.gc = gc;
-        this.rectangle.setFill(RandomGenerator.randomColor());
-        this.rectangle.setWidth(RandomGenerator.random(minW, maxW));
-        this.rectangle.setHeight(RandomGenerator.random(minH, maxH));
+public class MovingRectangle {
+    public static Rectangle generateRectangle(Pane movementArea, int minW, int maxW, int minH, int maxH){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(RandomGenerator.random(minW, maxW));
+        rectangle.setHeight(RandomGenerator.random(minH, maxH));
+        rectangle.setFill(RandomGenerator.randomColor());
+        rectangle.setTranslateX(RandomGenerator.random(0, (int) (movementArea.getMinWidth() - rectangle.getWidth())));
+        rectangle.setTranslateY(RandomGenerator.random(0, (int) (movementArea.getMinHeight() - rectangle.getHeight())));
+        movementArea.getChildren().add(rectangle);
+        return rectangle;
     }
 
-    @Override
-    public void run() {
-        x = RandomGenerator.random(0, WIDTH);
-        y = RandomGenerator.random(0, HEIGHT);
-       for(int i = 0; i < 100; i++){
-            MovingShape moving = MovingShape.randomMoving();
-            if (moving == MovingShape.up){
-                y -= distance;
-            } else if (moving == MovingShape.right){
-                x += distance;
-            } else if (moving == MovingShape.down){
-                y += distance;
-            } else if (moving == MovingShape.left){
-                x -= distance;
+    // Рассчитываем движение прямоугольника
+    public static void move(Rectangle rectangle, Pane movementArea, int numberMovements, int stepX, int stepY) {
+        for (int i = 0; i < numberMovements; i++){
+            if ((rectangle.getTranslateX() == 0) ||
+                    (rectangle.getTranslateX() == movementArea.getMinWidth() - rectangle.getWidth())) {
+                stepX *= -1;
             }
-            draw();
+            if ((rectangle.getTranslateY() == 0) ||
+                    (rectangle.getTranslateY() == movementArea.getMinHeight() - rectangle.getHeight())) {
+                stepY *= -1;
+            }
+            final double x = rectangle.getTranslateX() + stepX;
+            final double y = rectangle.getTranslateY() + stepY;
+            Platform.runLater(() -> {
+                rectangle.setTranslateX(x);
+                rectangle.setTranslateY(y);
+            });
             try {
-                Thread.sleep(300);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            gc.clearRect(x, y, rectangle.getWidth(), rectangle.getHeight());
         }
-
     }
 
-    private void draw(){
-        gc.setFill(rectangle.getFill());
-        gc.fillRect(x, y, rectangle.getWidth(), rectangle.getHeight());
+    // Рассчитываем движение прямоугольника. Метод для одного потока.
+    public static void move(ArrayList<Rectangle> rectangles, Pane movementArea, int numberMovements) {
+        ArrayList<Integer> stepsX = new ArrayList<>();
+        ArrayList<Integer> stepsY = new ArrayList<>();
+        for (int i = 0; i < rectangles.size(); i++){
+            stepsX.add(randomStep(new Random().nextBoolean()));
+            stepsY.add(randomStep(new Random().nextBoolean()));
+        }
+        for (int i = 0; i < numberMovements; i++){
+            for (int j = 0; j < rectangles.size(); j++){
+                move(rectangles.get(j), movementArea, 1, stepsX.get(j), stepsY.get(j));
+            }
+        }
     }
 
-    public int getDistance() {
-        return distance;
+    public static int randomStep(Boolean value){
+        if (value) return 1;
+        return -1;
     }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
 }
