9f2ffafde42a100d3526f9080c97ffa667c216e9
Sasha Kaminska <sasha249133@ukr.net>
2017-12-20 16:13:03 +0200

    add Random Generator

--- src/main/java/random/RandomGenerator.java
+++ src/main/java/random/RandomGenerator.java
@@ -10,12 +10,11 @@ public class RandomGenerator {
         return Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
     }
 
-    public static int randomNumber(int min, int max){
+    public static int random(int min, int max){
         return (int) (min + Math.random() * (max - min));
     }
 
-    public static double randomNumber(double min, double max){
+    public static double random(double min, double max){
         return min + Math.random() * (max - min);
     }
-
 }
